8c9e6300f133c4ad900f8947994d38483c65baf3
ioanniskalogridis <77626768+ioanniskalogridis@users.noreply.github.com>
2022-01-25 16:18:57 +0100

    Small changes

--- Bernoulli examples.R
+++ Bernoulli examples.R
@@ -1,6 +1,9 @@
 require(SemiPar)
 require(mgcv)
 require(ggplot2)
+require(mlbench)
+
+############################################# Birthweight and dysplasia #############################################################
 
 data(bpd)
 bpd <- bpd[order(bpd$birthweight),]
@@ -8,7 +11,7 @@ x <- bpd$birthweight
 y <- bpd$BPD
 plot(x,y)
 fit <- dpd(x, y, family = "b")
-fit.gam <- mgcv::gam(y~s(x, k = 40), family = binomial())
+fit.gam <- mgcv::gam(y~s(x, k = 40), family = binomial)
 
 lines(x, fit$fitted, lwd = 3, col = "blue", type = "l")
 lines(x, fit.gam$fitted.values, lwd = 3, col = "red")
@@ -24,3 +27,26 @@ p <- p + geom_line(data = df, aes(y = fit.dpd, x = x, group = 1), colour = "blue
 p <- p + scale_color_manual(values=c("gray", "purple"))
 p
 
+############################################ Pima Indian Diabetes ###################################################################
+
+data(PimaIndiansDiabetes)
+pima <- PimaIndiansDiabetes
+pima$diabetes <- ifelse(pima$diabetes == "pos", 1, 0)
+pima <- pima[order(pima$glucose),]
+x <- pima$glucose
+y <- pima$diabetes
+plot(x, y)
+fit <- dpd(x, y, family = "b")
+fit.gam <- mgcv::gam(y~s(x, k = 40), family = binomial)
+fit$alpha
+
+hist(fit$a.resids, xlab = "Anscombe residuals") # Anscombe residuals 
+
+outliers <- ifelse(abs(fit$a.resids)>2.6, 1, 0) # Anscombe residuals
+sum(outliers)
+df <- data.frame(x = x, y = y, fit.dpd =  fit$fitted, fit.gam = fit.gam$fitted.values, out = outliers )
+p <- ggplot(df, aes(x = x, y = y, colour = as.factor(out), shape = as.factor(out) ))  + labs(x = "Plasma glucose", y = "Probability of Diabetes")
+p <- p + geom_jitter(data = df, aes(y = y), height = 0.04, size = 4) + theme_bw(base_size = 35)+ guides(color = "none") + guides(shape = "none")
+p <- p + geom_line(aes(x = x, y = fit.dpd, group = 1), colour = "blue", linetype = 1, size = 1.4) + geom_line(aes(x = x, y = fit.gam, group = 1), colour = "red", linetype = 2, size = 1.4)
+p <- p + scale_color_manual(values=c("gray", "purple"))
+p
--- Gaussian examples.R
+++ Gaussian examples.R
@@ -2,6 +2,8 @@ require(SemiPar)
 require(quantreg)
 require(MASS)
 
+
+########################################################## LIDAR ###############################################################
 data(lidar)
 y <- lidar$logratio
 x <- lidar$range
@@ -13,6 +15,8 @@ lines(x, fit.GIC$fitted, lwd = 4, col = "darkgray",)
 legend("bottomleft", legend = c("DPD with AIC", "DPD with GIC"), lwd = c(4,4), col = c("blue", "darkgray"), cex = 1.4)
 fit.AIC$alpha
 
+######################################################### Motorcycle impact ####################################################
+
 y <- mcycle$accel
 x <- mcycle$times
 plot(x, y, cex = 1.2, pch = 19) ; grid()
@@ -20,14 +24,18 @@ fit.mcycle <- dpd(x, y, family = "g")
 lines(x, fit.mcycle$fitted, lwd = 4, col = "blue")
 fit.mcycle$alpha
 
+######################################################## Age and income ########################################################
+
 data("age.income")
 age.income <- age.income[order(age.income$age),]
 x <- age.income$age
 y <- age.income$log.income
 plot(x, y, cex = 1.2, pch = 19, xlab = "Income", ylab = "Age") ; grid()
 fit.ai <- dpd(x, y, family= "g")
-lines(x, fit.ai$est, lwd = 4, col = "blue")
 fit.ai$alpha
+lines(x, fit.ai$fitted, lwd = 4, col = "blue")
+
+######################################################## Mammals weight and speed ###############################################
 
 data("Mammals")
 Mammals <- Mammals[order(Mammals$weight), ]
@@ -35,17 +43,18 @@ Mammals$weight <- log(Mammals$weight)
 Mammals$speed <- log(Mammals$speed)
 x <- Mammals$weight
 y <- Mammals$speed
-plot(x, y, cex = 1.2, pch = 19)
+plot(x, y, cex = 1.2, pch = 19, xlab = "Weight (log)", ylab = "Speed (log)")
 fit.mamAIC <- dpd(x, y, sel = "AIC")
 fit.mamGIC <- dpd(x, y, sel = "GIC")
 fit.gam <- gam(y~s(x, k = 40, bs = "cr"))
 lines(x, fit.mamAIC$fitted, lwd = 4, col = "blue")
 lines(x, fit.mamGIC$fitted, lwd = 4, col = "darkgray")
 lines(x, fit.gam$fitted.values, lwd = 4, col = "red", lty = 2)
-legend("bottomright", legend = c("DPD with AIC", "DPD with GIC", "GAM"), lwd = c(4,4, 4), lty = c(1,1, 2),
+legend("bottomright", legend = c("DPD with AIC", "DPD with GIC", "GAM"), lwd = c(4,4, 4), lty = c(1, 1, 2),
        col = c("blue", "darkgray", "red"), cex = 1.4)
 fit.mamGIC$alpha
 
-hist(fit.mamGIC$p.resids, col = "gray") 
+
+#hist(fit.mamGIC$p.resids, col = "gray") 
 # Numerous outliers
 
