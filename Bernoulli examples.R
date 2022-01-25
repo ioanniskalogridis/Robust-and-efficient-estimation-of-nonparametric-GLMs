58600062ab8874012f989c56b1863c21abfdbb7a
ioanniskalogridis <77626768+ioanniskalogridis@users.noreply.github.com>
2022-01-25 14:45:15 +0100

    Update Bernoulli examples.R

--- Bernoulli examples.R
+++ Bernoulli examples.R
@@ -17,10 +17,10 @@ fitbpd$alpha
 outliers <- ifelse(abs(fit$a.resids)>2.6, 1, 0) # Anscombe residuals
 sum(outliers)
 
-df <- data.frame(xb = as.numeric(x), yb = y, fit.dpd =  c(fit$fitted), fit.gam = fit.gam$fitted.values, out = outliers )
-p <- ggplot(data = df, aes(x = xb, y = yb, colour = as.factor(out), shape = as.factor(out) )) + labs(x = "Birthweight", y = "Probability of dysplasia")
-p <- p + geom_jitter(data = df, aes(y = yb), height = 0.04, size = 4) + theme_bw(base_size = 35) + guides(color = "none") + guides(shape = "none")
-p <- p + geom_line(data = df, aes(y = fit.dpd, x = xb, group = 1), colour = "blue", linetype = 1, size = 1.4) # + geom_line(aes(x = x, y = fit.gam), colour = "red", linetype = 2, size = 1.4)
+df <- data.frame(x = x, y = y, fit.dpd =  c(fit$fitted), fit.gam = fit.gam$fitted.values, out = outliers )
+p <- ggplot(data = df, aes(x = x, y = y, colour = as.factor(out), shape = as.factor(out) )) + labs(x = "Birthweight", y = "Probability of dysplasia")
+p <- p + geom_jitter(data = df, aes(y = y), height = 0.04, size = 4) + theme_bw(base_size = 35) + guides(color = "none") + guides(shape = "none")
+p <- p + geom_line(data = df, aes(y = fit.dpd, x = x, group = 1), colour = "blue", linetype = 1, size = 1.4) + geom_line(aes(x = x, y = fit.gam, group = 1), colour = "red", linetype = 2, size = 1.4)
 p <- p + scale_color_manual(values=c("gray", "purple"))
 p
 
